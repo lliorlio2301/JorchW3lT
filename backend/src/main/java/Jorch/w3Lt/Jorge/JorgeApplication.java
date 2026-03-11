688bb6b30a19774e186066069c5a9a25a3867d02
lliorlio2301 <jorge2301@outlook.es>
2026-03-11 07:56:56 +0100

    Neuer initialer Commit - Spring Boot backend structure and project guidelines

---
+++ backend/src/main/java/Jorch/w3Lt/Jorge/JorgeApplication.java
@@ -0,0 +1,13 @@
+package Jorch.w3Lt.Jorge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JorgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JorgeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Jorch/w3Lt/Jorge/JorgeApplicationTests.java
@@ -0,0 +1,13 @@
+package Jorch.w3Lt.Jorge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JorgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
