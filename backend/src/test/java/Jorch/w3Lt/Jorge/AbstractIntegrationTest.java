c036613fd6b4b9e6a8e8d3be9250a0832eea973b
lliorlio2301 <jorge2301@outlook.es>
2026-03-15 12:09:27 +0100

    test(backend): setup base class for Testcontainers integration tests

---
+++ backend/src/test/java/Jorch/w3Lt/Jorge/AbstractIntegrationTest.java
@@ -0,0 +1,17 @@
+package Jorch.w3Lt.Jorge;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+}
